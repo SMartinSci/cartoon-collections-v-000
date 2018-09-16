f1a54fed486e9cd5dfb802f4d462d6fbd6f4a027
Sherry Martin <sm674@cornell.edu>
2018-09-16 23:14:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(planeteer_calls)
- planeteer_calls.collect.capitalize { |x| x + "!" }
+ planeteer_calls.collect.upcase { |x| x + "!" }
 end
 
 def long_planeteer_calls# code an argument here
